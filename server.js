16750c5456a15d4640f8b60d18899d361bc08d08
adarshbs311 <adarshbs311@gmail.com>
2016-10-08 20:20:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: "article-one",
+    heading:"article1",
+    date:"8/10/2016",
+    content:` <p>
+                Hello, this is my first html page.How do you do?
+                Hello, this is my first html page.How do you do?
+                Hello, this is my first html page.How do you do?
+            </p>
+            
+            <p>
+                Hello, this is my first html page.How do you do?
+                Hello, this is my first html page.How do you do?
+                Hello, this is my first html page.How do you do?
+            </p>
+            
+            <p>
+                Hello, this is my first html page.How do you do?
+                Hello, this is my first html page.How do you do?
+                Hello, this is my first html page.How do you do?
+            </p>`
+};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmlTemplate=`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+                ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+        ${content}
+        </div>
+        </div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
