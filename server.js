a1917f5fdf18e3a148f9a1afa9a630abbc0a9194
adarshbs311 <adarshbs311@gmail.com>
2016-11-17 23:43:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles ={
-    'article-one' :{
+    'article1' :{
     title: "article-one",
     heading:"article1",
     date:"8/10/2016",
